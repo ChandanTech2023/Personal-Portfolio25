51a6937390e50482e6a257fcbafddfc112cd7ca0
Chandan <chandankumar13102005@gmail.com>
2025-06-23 03:30:23 -0700

    Theme customization with utility container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,15 @@ const Home = () => {
       <header>
         <h1>Welcome to My React App!</h1>
       </header>
-      <main>
-        <p>This is the home page of your application.</p>
-        <p>You can add more content and components here.</p>
-      </main>
-      <footer>
-        <p>&copy; 2025 My React App</p>
-      </footer>
+      {/* Theme Toggle */}
+      
+      {/* Background Effect  */}
+
+      {/* Navbar  */}
+
+      {/* Main Content */}
+
+      {/* Footer  */}
     </div>
   );
 };
