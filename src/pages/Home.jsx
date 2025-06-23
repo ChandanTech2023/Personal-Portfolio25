20f23531dd8894824c8601419286e71d228e654b
Chandan <chandankumar13102005@gmail.com>
2025-06-23 00:27:20 -0700

    Configure tailwindCss in portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My React App!</h1>
+      </header>
+      <main>
+        <p>This is the home page of your application.</p>
+        <p>You can add more content and components here.</p>
+      </main>
+      <footer>
+        <p>&copy; 2025 My React App</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,4 @@
+const NotFound = ()=>{
+    return <div>Hello </div>
+}
+ export default NotFound
